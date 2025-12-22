d765be6440ea24935d465e47685563291e1a7e27
Liao-ZH <liaozhenghan@qq.com>
2025-12-22 23:27:41 +0800

    added settingManager

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export class CONSTANTS {
+    // 插件信息
+    public static readonly PLUGIN_NAME: string = "inbox-light";
+    public static readonly PLUGIN_NAME_ZH: string = "收件箱简化";
+
+    // 插件设置
+    public static readonly SETTING_STORAGE: string = "menu_config";
+    public static readonly SETTING_STORAGE_HEIGHT: string = "400px";
+    public static readonly SETTING_KEY_INBOXDOCID: string = "inboxDocId";
+    public static readonly SETTING_KEY_REPLACEBUILTIN: string = "replaceBuiltIn";
+
+    // HTML元素样式
+    public static readonly STYLE_DISABLED_OPACITY: string = "0.5";
+}
---
+++ src/worker/settingManager.ts
@@ -0,0 +1,95 @@
+// 管理设置
+import { showMessage } from "siyuan";
+import { SettingUtils } from "@/libs/setting-utils";
+import { getPluginInstance } from "@/utils/pluginInstance";
+import { CONSTANTS as C } from "@/constants";
+import { getHPathByID } from "@/utils/api";
+import * as logger from "@/utils/logger";
+
+/**
+ * 管理插件设置
+ */
+export class SettingManager {
+    private settingUtils: SettingUtils;
+    private plugin = getPluginInstance();
+
+    constructor() {
+        // 初始化设置
+        this.initSettingUtils();
+        // 从存储加载设置
+        this.settingUtils.load();
+    }
+
+    /**
+     * 初始化设置工具
+     */
+    initSettingUtils() {
+        const i18nSetting = this.plugin.i18n.setting;
+
+        // 设置工具初始化
+        this.settingUtils = new SettingUtils({
+            plugin: this.plugin,
+            name: C.SETTING_STORAGE,
+            height: C.SETTING_STORAGE_HEIGHT,
+            callback: (data) => {
+                logger.logDebug("设置完成", data);
+            }
+        });
+
+        // 添加设置项
+        this.settingUtils.addItem({
+            key: C.SETTING_KEY_INBOXDOCID,
+            value: "",
+            type: "textinput",
+            title: i18nSetting[C.SETTING_KEY_INBOXDOCID]["title"],
+            description: i18nSetting[C.SETTING_KEY_INBOXDOCID]["description"],
+            action: {
+                callback: async () => {
+                    let value = await this.settingUtils.takeAndSave(C.SETTING_KEY_INBOXDOCID);
+                    logger.logDebug(`设置：${C.SETTING_KEY_INBOXDOCID}`, value);
+
+                    // // 尝试更新描述文本，但不大成功
+                    // const textElem = document.querySelector('div.b3-label__text');
+                    // logger.logDebug(`textElem: ${textElem?.innerHTML}`)
+                    // const hint = i18nSetting[C.SETTING_KEY_INBOXDOCID]
+                    // if (textElem && textElem.innerHTML.includes(hint["description"])) {
+                    //     const hpath = await getHPathByID(value);
+                    //     if (hpath) {
+                    //         textElem.innerHTML = `${hint["description"]} ${hint["hpathHint"]}${hpath}`;
+                    //     } else {
+                    //         textElem.innerHTML = `${hint["description"]} ${hint["hpathHint"]}${hint["hpathInvaid"]}`;
+                    //     }
+                    // }
+                    const hint = i18nSetting[C.SETTING_KEY_INBOXDOCID]
+                    const hpath = await getHPathByID(value);
+                    if (hpath) {
+                        showMessage(`${hint["hpathHint"]}${hpath}`, 0, "info");
+                    }
+                }
+            }
+        });
+        this.settingUtils.addItem({
+            key: C.SETTING_KEY_REPLACEBUILTIN,
+            value: false,
+            type: "checkbox",
+            title: i18nSetting[C.SETTING_KEY_REPLACEBUILTIN]["title"],
+            description: i18nSetting[C.SETTING_KEY_REPLACEBUILTIN]["description"],
+            action: {
+                callback: () => {
+                    let value = !this.settingUtils.get(C.SETTING_KEY_REPLACEBUILTIN);
+                    this.settingUtils.setAndSave(C.SETTING_KEY_REPLACEBUILTIN, value);
+                    logger.logDebug(`设置：${C.SETTING_KEY_REPLACEBUILTIN}`, value);
+                }
+            }
+        });
+    }
+
+    /**
+     * 获取设置值
+     * @param key 设置键
+     * @returns 设置值
+     */
+    get(key: string) {
+        return this.settingUtils.get(key);
+    }
+}
